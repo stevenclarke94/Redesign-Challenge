50398147aeac15511f12842ebf7d3371c1442cef
stevenclarke94 <61111250+stevenclarke94@users.noreply.github.com>
2020-12-20 20:42:19 +0000

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function load_stylesheets(){
+
+
+
+
+	wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', 
+array(), false, 'all');
+	wp_enqueue_style('bootstrap');
+
+
+	wp_register_style('style', get_template_directory_uri() . '/style.css', 
+array(), false, 'all');
+	wp_enqueue_style('style');
+}
+
+add_action('wp_enqueue_scripts', 'load_stylesheets');
