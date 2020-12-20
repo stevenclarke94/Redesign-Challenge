9e4d3daf137310b9ce16d8a8967bf44ed49d6d08
stevenclarke94 <61111250+stevenclarke94@users.noreply.github.com>
2020-12-20 20:41:27 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<?php wp_head();?>
+
+</head>
+
+<body <?php body_class();?>>
+<nav style="display: flex;">
+	<div class="nav-item">
+		<div class="menuLine1"></div>
+	 	<div class="menuLine2"></div>
+	</div>
+	<div>
+		<img style="width:140px;" src="http://127.0.0.1:81/wordpress/wp-content/uploads/2020/12/logoedit.png" alt="logo">
+	 
+	</div>
+	<div class="en-symbol">
+	 <b>En</b>
+	</div>
+</nav>
