30cb38074d0dfbdf69608bbd6946551059bcba5c
stevenclarke94 <61111250+stevenclarke94@users.noreply.github.com>
2020-12-20 20:42:43 +0000

    Add files via upload

---
+++ front-page.php
@@ -0,0 +1,76 @@
+<?php get_header();?>
+<div class="full-container">
+	<div class="row1">
+		<div class="colfirst">
+				
+			</div>
+			<div class="collpush">
+				
+			</div>
+			<div class="collFace1">
+				
+			</div>
+			<div class="collFace2">
+				
+			</div>
+			<div class="collpush">
+				
+			</div>
+			<div class="colLast">
+				
+			</div>
+	</div>
+	<div class="row2">
+			<div class="colfirst">
+				<p class="sideTextLeft">Scroll<br> to navigate</p>
+			</div>
+			<div class="collpush">
+				
+			</div>
+			<div class="coll">
+				<div class="pageRow">
+					<div class="hoverLine">
+						<div class="pageCol"><p>About<br> MCG</p></div>
+						<div class="pageCol2"> <p>01</p></div>
+					</div>
+				</div>
+			</div>
+			<div class="coll">
+				<div class="pageRow">
+					<div class="hoverLine">
+						<div class="pageCol"><p>Our<br> Brands</p></div>
+						<div class="pageCol2"> <p>02</p></div>
+					</div>
+				</div>
+			</div>
+			<div class="collpush">
+				<div class="pageRow">
+					<div class="hoverLine">
+						<div class="pageCol"><p>Explore<br> Careers</p></div>
+						<div class="pageCol2"> <p>03</p></div>
+					</div>
+				</div>
+			</div>
+			<div class="colLast">
+				<p class="sideTextRight">Welcome to MCG</p>
+				<div class="socials">
+					<img src="http://127.0.0.1:81/wordpress/wp-content/uploads/2020/12/facebook.png">
+					<img src="http://127.0.0.1:81/wordpress/wp-content/uploads/2020/12/instagram.png">
+					<img src="http://127.0.0.1:81/wordpress/wp-content/uploads/2020/12/twitter.png">
+				</div>
+
+			</div>
+</div>
+</div>
+<div class="frontHeading">
+	<p>01 ----- 02 Welcome</p>
+	<h1>Creating Human <br>engagement.</h1>
+	<p style="font-size:1.5rem; margin-bottom: 40px;">through technology.</p>
+	<a style="font-size:0.7rem;letter-spacing: 2px; color:#2125299e;"href="/"><img style="width:20px;" src="http://127.0.0.1:81/wordpress/wp-content/uploads/2020/12/arrow.png"> Discover MCG</a>
+</div>
+
+<div class="fadedTitle">
+<h1 style="font-size: 7rem; margin-left:10px;">Human</h1><h1 style="margin-top: 300px; margin-right:0px; font-size: 7rem;">Engagement</h1>
+</div>
+<img class="frontImg" src="http://127.0.0.1:81/wordpress/wp-content/uploads/2020/12/mcg-landing.png">
+<?php get_footer();?>
